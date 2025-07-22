5a6716588a36f417e6ba3a3c2442850d1c5b8801
halidigitulo <halididoank@gmail.com>
2025-07-23 06:49:08 +0800

    update Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,7 +24,8 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             MenuPermissionSeeder::class,
             ProfileSeeder::class,
-            RolePermissionSeeder::class
+            RolePermissionSeeder::class,
+            ModelHasRoleSeeder::class
         ]);
     }
 }
